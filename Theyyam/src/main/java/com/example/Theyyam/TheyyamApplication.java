1a4fa9417b24557cebd548f4ffaec43f92367702
joshitha <joshithasunil364@gmail.com>
2023-12-28 15:50:19 +0530

    initial structure is crested

---
+++ Theyyam/src/main/java/com/example/Theyyam/TheyyamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Theyyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheyyamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheyyamApplication.class, args);
+	}
+
+}
---
+++ Theyyam/src/test/java/com/example/Theyyam/TheyyamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Theyyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheyyamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
